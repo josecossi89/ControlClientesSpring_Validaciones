01cf1dbf9dbc607d9659dbaa4053cf81eb3d869c
COSSIGOMEZ <COSSIGOMEZ@192.168.0.33>
2021-04-01 07:01:19 -0300

    Validaciones en el formulario

---
+++ src/main/java/com/vzla/jc/domain/Persona.java
@@ -0,0 +1,29 @@
+package com.vzla.jc.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "persona")
+public class Persona implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPersona;
+
+    @NotEmpty
+    private String nombre;
+
+    @NotEmpty
+    private String apellido;
+
+    @NotEmpty
+    private String email;
+    private String telefono;
+
+}
